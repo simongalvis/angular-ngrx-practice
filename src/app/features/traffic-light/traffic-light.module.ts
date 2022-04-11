9bee7601a811e2351e92daee24234d74c14ce485
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-11 15:00:33 -0400

    basic routing for traffic light setup

--- src/app/features/traffic-light/traffic-light.module.ts
+++ src/app/features/traffic-light/traffic-light.module.ts
@@ -1,17 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LightComponent } from './components/light/light.component';
 import { TrafficLightComponent } from './traffic-light.component';
 
 const routes: Routes = [
   {
     path: '', // ?? It's up to the app module what to call this.
     component: TrafficLightComponent,
+    children: [
+      {
+        path: 'light',
+        component: LightComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'light',
+      },
+    ],
   },
 ];
 
 @NgModule({
-  declarations: [TrafficLightComponent],
+  declarations: [TrafficLightComponent, LightComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
