d981b331d99e0ee7113bd8bbc5b2d5d02e4cb863
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-12 11:11:45 -0400

    before removing subscriptions

--- src/app/features/traffic-light/components/counter/counter.component.ts
+++ src/app/features/traffic-light/components/counter/counter.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { filter, map, Observable, Subscription } from 'rxjs';
 import { selectCounterCurrent } from '../../state';
 import * as actions from '../../state/actions/counter.actions';
 @Component({
@@ -8,12 +8,28 @@ import * as actions from '../../state/actions/counter.actions';
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.css'],
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, OnDestroy {
   count$!: Observable<number>;
+  subscription: Subscription[] = [];
   constructor(private store: Store) {}
+  ngOnDestroy(): void {
+    this.subscription.forEach((s) => s.unsubscribe());
+  }
 
   ngOnInit(): void {
-    this.count$ = this.store.select(selectCounterCurrent);
+    this.count$ = this.store
+      .select(selectCounterCurrent)
+      .pipe(filter((num) => num % 2 === 0));
+    // if you are doing this, you are most likely doing something wrong.
+    const sub = this.store
+      .select(selectCounterCurrent)
+      .pipe(
+        filter((num) => num % 2 === 0), // if this is true, continue, otherwise, stop here. always going to event
+        map((even) => ({ message: `Even Number!`, value: even })) // even is a number that is even, a string
+      )
+      .subscribe((c) => console.log(c));
+
+    this.subscription.push(sub);
   }
 
   increment() {
