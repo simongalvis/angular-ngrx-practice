759214d226da80b9deed37587033739a25d4b110
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-13 11:11:40 -0400

    before break!

--- src/app/features/todo/components/entry/entry.component.ts
+++ src/app/features/todo/components/entry/entry.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { todoItemCreated } from '../../state/actions/todos.events';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
-  styleUrls: ['./entry.component.css']
+  styleUrls: ['./entry.component.css'],
 })
 export class EntryComponent implements OnInit {
+  form = this.formBuilder.group({
+    description: [
+      '',
+      [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
+    ],
+  });
+  constructor(private formBuilder: FormBuilder, private store: Store) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get description() {
+    return this.form.get('description');
   }
 
+  ngOnInit(): void {}
+
+  submit(ctrl: HTMLInputElement) {
+    if (this.form.valid) {
+      const description = this.description?.value;
+      this.store.dispatch(todoItemCreated({ description }));
+      this.form.reset();
+      ctrl.focus();
+    } else {
+      // can't dispatch.
+    }
+  }
 }
